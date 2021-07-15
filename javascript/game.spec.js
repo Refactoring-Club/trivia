85a1df0b1e720d3409ce6d5f97f008af845dc223
India Amos <india.amos@onepeloton.com>
2021-07-14 20:18:24 -0400

    Add more tests around number of players

--- javascript/game.spec.js
+++ javascript/game.spec.js
@@ -10,6 +10,16 @@ describe("The test environment", function () {
   });
 });
 
+describe("When there are no players", function () {
+  it("tells you that you did a bad", () => {
+    var game = new Game();
+
+    const rollTheDie = () => game.roll(Math.floor(Math.random() * 6) + 1);
+
+    expect(rollTheDie).toThrow("You did a bad.");
+  });
+});
+
 describe("When there is only one player", function () {
   it("tells you that you did a bad", () => {
     var game = new Game();
@@ -18,5 +28,17 @@ describe("When there is only one player", function () {
     const rollTheDie = () => game.roll(Math.floor(Math.random() * 6) + 1);
 
     expect(rollTheDie).toThrow("You did a bad.");
+  });
+});
+
+describe("When there enough players", function () {
+  it("doesn't throw a bad error", () => {
+    var game = new Game();
+
+    game.add("Chet");
+    game.add("Marla");
+    const rollTheDie = () => game.roll(Math.floor(Math.random() * 6) + 1);
+
+    expect(rollTheDie).not.toThrow();
   });
 });
